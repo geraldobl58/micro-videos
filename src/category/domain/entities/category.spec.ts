a10b05de82ed6bd2ba67d26d7b7df66e8d135a3a
Geraldo Luiz <geraldobl58@gmail.com>
2022-12-17 17:33:17 -0300

    test: Category entity constructor test

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -3,9 +3,26 @@ import { Category } from "./category"
 describe("Category Unit Tests", () => {
 
   test("constructor of category", () => {
-    const category = new Category('Movie')
+    const created_at = new Date;
 
-    expect(category.name).toBe('Movie')
+    const category = new Category({
+      name: 'Movie',
+      description: 'Description',
+      is_active: true,
+      created_at
+    })
+
+    expect(category.props).toStrictEqual({
+      name: 'Movie',
+      description: 'Description',
+      is_active: true,
+      created_at
+    })
+
+    // expect(category.name).toBe('Movie');
+    // expect(category.description).toBe('Description');
+    // expect(category.is_active).toBeTruthy();
+    // expect(category.created_at).toBe(created_at);
   })
   
 })
